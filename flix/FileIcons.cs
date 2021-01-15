193f02553e34d5d86b223f70447d1736725613c6
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2021-01-15 19:48:02 +0100

    Add project files.

---
+++ flix/FileIcons.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flix
+{
+    public static class FileIcons
+    {
+        [StructLayout( LayoutKind.Sequential )]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public IntPtr iIcon;
+            public uint dwAttributes;
+            [MarshalAs( UnmanagedType.ByValTStr, SizeConst = 260 )]
+            public string szDisplayName;
+            [MarshalAs( UnmanagedType.ByValTStr, SizeConst = 80 )]
+            public string szTypeName;
+        };
+
+        class Win32
+        {
+            public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
+            public const uint SHGFI_SMALLICON = 0x1; // 'Small icon
+            public const uint SHGFI_ADDOVERLAYS = 0x20;
+            public const uint SHGFI_ICON = 0x100;
+            public const uint SHGFI_SELECTED = 0x10000;
+
+            [DllImport( "shell32.dll" )]
+            public static extern IntPtr SHGetFileInfo( string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags );
+
+            [DllImport( "User32.dll" )]
+            public static extern int DestroyIcon( IntPtr hIcon );
+        }
+
+        static FileIcons()
+        {
+            // Do nothing
+        }
+
+        public static Icon GetSmallIcon( string fileName, bool selected = false )
+        {
+            return GetIcon( fileName, Win32.SHGFI_SMALLICON | ( selected ? Win32.SHGFI_SELECTED : 0 ) );
+        }
+
+        public static Icon GetLargeIcon( string fileName, bool selected = false )
+        {
+            return GetIcon( fileName, Win32.SHGFI_LARGEICON | ( selected ? Win32.SHGFI_SELECTED : 0 ) );
+        }
+
+        private static Icon GetIcon( string fileName, uint flags )
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            IntPtr hImgSmall = Win32.SHGetFileInfo( fileName, 0, ref shinfo, (uint) Marshal.SizeOf( shinfo ), Win32.SHGFI_ICON | flags );
+
+            Icon icon = (Icon) Icon.FromHandle( shinfo.hIcon ).Clone();
+            Win32.DestroyIcon( shinfo.hIcon );
+            return icon;
+        }
+    }
+}
---
+++ flix/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flix
+{
+    public partial class MainForm : Form
+    {
+        private readonly string defaultLocation = Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments );
+
+        private readonly List<string> directoryHistory = new List<string>();
+
+        public MainForm( string[] args )
+        {
+            InitializeComponent();
+
+            var location = defaultLocation;
+            if ( args.Length > 0 )
+            {
+                var suppliedLocation = args[ 0 ];
+                if ( Directory.Exists( suppliedLocation ) )
+                {
+                    location = suppliedLocation;
+                }
+                else if ( File.Exists( suppliedLocation ) )
+                {
+                    location = Directory.GetParent( suppliedLocation ).FullName;
+                }
+            }
+
+            OpenDirectoryView( location );
+        }
+
+        private void OpenDirectoryView( string location )
+        {
+            textLocation.Text = location;
+
+            try
+            {
+                listBrowser.BeginUpdate();
+                listBrowser.Items.Clear();
+
+                foreach ( var file in Directory.GetFiles( location ) )
+                {
+                    var fInfo = new FileInfo( file );
+                    var length = Math.Ceiling( (double) fInfo.Length / 1024 );
+                    var fileType = FileTypes.GetFileTypeDescription( fInfo.FullName );
+
+                    if ( !imagesSmall.Images.ContainsKey( fInfo.FullName ) )
+                    {
+                        imagesSmall.Images.Add( fInfo.FullName, FileIcons.GetSmallIcon( fInfo.FullName ) );
+                    }
+
+                    var lfItem = new ListViewItem();
+                    lfItem.Tag = fInfo;
+                    lfItem.Text = fInfo.Name;
+                    lfItem.ImageKey = fInfo.FullName;
+
+                    lfItem.SubItems.Add( new ListViewItem.ListViewSubItem( lfItem, $"{fInfo.LastWriteTime:g}" ) );
+                    lfItem.SubItems.Add( new ListViewItem.ListViewSubItem( lfItem, $"{length:N0} KB" ) );
+                    lfItem.SubItems.Add( new ListViewItem.ListViewSubItem( lfItem, fileType ) );
+
+                    listBrowser.Items.Add( lfItem );
+                }
+            }
+            finally
+            {
+                listBrowser.EndUpdate();
+            }
+        }
+    }
+}
